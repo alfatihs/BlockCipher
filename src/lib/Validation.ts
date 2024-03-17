3c979a2a5ca231141c3c8e1d4839bfc4bf5c0dbd
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2024-03-17 17:15:45 +0700

    comment ciphertype

--- src/lib/Validation.ts
+++ src/lib/Validation.ts
@@ -8,24 +8,24 @@ export interface UploadPayload {
 }
 
 export function getCipher(cipher: string) {
-  switch (cipher) {
-    case "hill":
-      return CipherType.Hill;
-    case "super":
-      return CipherType.SuperEncryption;
-    case "affine":
-      return CipherType.Affine;
-    case "playfair":
-      return CipherType.Playfair;
-    case "vigenere":
-      return CipherType.VigenereStandard;
-    case "vigenere-extended":
-      return CipherType.VigenereExtended;
-    case "vigenere-autokey":
-      return CipherType.VigenereAutokey;
-    default:
-      throw new ValidationError("Invalid cipher.");
-  }
+  // switch (cipher) {
+  //   case "hill":
+  //     return CipherType.Hill;
+  //   case "super":
+  //     return CipherType.SuperEncryption;
+  //   case "affine":
+  //     return CipherType.Affine;
+  //   case "playfair":
+  //     return CipherType.Playfair;
+  //   case "vigenere":
+  //     return CipherType.VigenereStandard;
+  //   case "vigenere-extended":
+  //     return CipherType.VigenereExtended;
+  //   case "vigenere-autokey":
+  //     return CipherType.VigenereAutokey;
+  //   default:
+  //     throw new ValidationError("Invalid cipher.");
+  // }
 }
 
 export function getAndValidateUpload(formData: FormData): UploadPayload {
